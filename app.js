13f9721b70c49ede74ea6572ad86ea61e3d848c9
许浩星 <xuhx2@wedoctor.com>
2022-11-22 16:04:25 +0800

    feat：改写app.js（注册中间件）

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
+// 实例化 Koa 应用实例
 const Koa = require('koa')
 const app = new Koa()
 
-app.use(async ctx => {
-  ctx.body = 'Hello haoxing'
+// 中间件
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+const cors = require('koa-cors')
+const router = require('./routes/index')
+
+// 注册 error
+onerror(app)
+// 注册 bodyparser
+app.use(bodyparser())
+// 注册日志
+app.use(logger())
+// 注册静态资源
+app.use(require('koa-static')(__dirname + '/public'))
+// 注册跨域
+app.use(cors())
+// 注册自定义中间件
+require('./middlewares/index')(app)
+// 注册路由
+app.use(router.routes(), router.allowedMethods())
+
+// logger-handling
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
 })
 
-app.listen(3000)
-console.log('Node服务器开启，监听在3000端口');
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx);
+})
+
+module.exports = app
